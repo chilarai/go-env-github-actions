28526b2715e9d8ea186a04f466d03b532038984d
chilarai <chilly5476@gmail.com>
2022-06-06 10:46:11 +0530

    Inital commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-env-github-actions
+
+go 1.15
+
+require github.com/joho/godotenv v1.4.0
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+}
+
+func main(){
+	fmt.Println("Go env github actions")
+
+	env1, err1 := os.LookupEnv("TEST1")
+	log.Println(env1, err1)
+
+	if !err1 {
+		fmt.Println("Could not read TEST1 from env")
+	} else {
+		fmt.Println("Value of Test1", env1)
+	}
+}
